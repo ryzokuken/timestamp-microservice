55286ba5a6cb3e76fba968c1c0ee18980f10a7aa
Ujjwal Sharma <usharma1998@gmail.com>
2016-11-29 07:09:23 +0000

    Fixed port

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.get('/:date', function (req, res) {
     }
 });
 
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!');
+app.listen(process.env.PORT, function () {
+    console.log('Example app listening on port ' + process.env.PORT + '!');
 });
