bce766e22845683c378bd1fbdf3fd5b97d37bded
Ujjwal Sharma <usharma1998@gmail.com>
2016-11-29 06:52:28 +0000

    Finalized Functionality

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var path = require('path');
+var moment = require('moment');
 
 app.get('/', function (req, res) {
-  res.send('Hello FCC!')
-})
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/:date', function (req, res) {
+    var date = req.params.date;
+    if (+date >= 0) {
+        res.json({
+            unix: date,
+            natural: moment.unix(+date).format('MMMM DD, YYYY')
+        });
+    } else {
+        if (moment(date, 'MMMM DD, YYYY').isValid()) {
+            res.json({
+                unix: moment(date).format('X'),
+                natural: date
+            });
+        } else {
+            res.json({
+                unix: null,
+                natural: null
+            });
+        }
+    }
+});
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
-})
+    console.log('Example app listening on port 8080!');
+});
